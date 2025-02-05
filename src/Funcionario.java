aa10174b892067b6c91692f012b7a2487eed9b71
Edielson062 <129564714+Edielson062@users.noreply.github.com>
2025-02-04 21:50:02 -0300

    Add files via upload

---
+++ src/Funcionario.java
@@ -0,0 +1,36 @@
+public class Funcionario {
+    private String nome;
+    private double salario;
+
+    public Funcionario() {
+    }
+
+    public Funcionario(String nome, double salario) {
+        this.nome = nome;
+        this.salario = salario;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Double aumentarSalario(Double aumento) {
+        return salario + aumento;
+    }
+
+    public String toString() {
+        return "Funcionario{" + "nome= " + nome + ", salario= " + salario + '}';
+    }
+}
